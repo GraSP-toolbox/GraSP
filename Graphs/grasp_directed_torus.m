5214d96e06de55c85feed354232603be31b44ca5
Benjamin Girault <benjamin.girault@usc.edu>
2016-11-11 11:29:29 -0800

    Directed Torus: one argument for equal dimensions

--- Graphs/grasp_directed_torus.m
+++ Graphs/grasp_directed_torus.m
@@ -5,11 +5,15 @@
 %
 % Authors:
 %  - Benjamin Girault <benjamin.girault@ens-lyon.fr>
+%  - Benjamin Girault <benjamin.girault@usc.edu>
 
 % Copyright Benjamin Girault, École Normale Supérieure de Lyon, FRANCE /
-% Inria, FRANCE (2015-11-01)
+% Inria, FRANCE (2015)
+% Copyright Benjamin Girault, University of Sourthern California, Los
+% Angeles, California, USA (2016)
 % 
 % benjamin.girault@ens-lyon.fr
+% benjamin.girault@usc.edu
 % 
 % This software is a computer program whose purpose is to provide a Matlab
 % / Octave toolbox for handling and displaying graph signals.
@@ -42,6 +46,9 @@
 
 function graph = grasp_directed_torus(N, M)
     %% Size of the new graph
+    if nargin == 1
+        M = N;
+    end
     NA = N * M;
     
     %% Initialization
