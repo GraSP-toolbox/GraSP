b27fcfbd1e95142446b801991d14043e08eb651d
Benjamin Girault <benjamin.girault@usc.edu>
2018-03-01 15:50:58 -0800

    Subgraph code factorization

--- Graphs/Tools/grasp_subgraph.m
+++ Graphs/Tools/grasp_subgraph.m
@@ -1,22 +1,20 @@
-%Selects the subgraph associated to the biggest connected component of a
-%graph.
+%Computes a subgraph.
 %
-%   graph = GRASP_BIGGEST_CONNECTED_COMPONENT(graph) find the biggest
-%   connected component and returns it.
+%NOTE: this function only updates the standard fields of the graph
+%structure (see GRASP_STRUCT).
 %
-%   [..., nodes] = GRASP_BIGGEST_CONNECTED_COMPONENT(...) also returns
-%   the set of selected nodes.
+%   g = GRASP_SUBGRAPH(g, vertex_set) remove the vertex not in vertex_set
+%   from the graph g.
+%
+%   [..., mapping] = GRASP_SUBGRAPH(...) also outputs the mapping from the
+%   old vertex index to the new vertex index (0 if not included).
 %
 % Authors:
-%  - Benjamin Girault <benjamin.girault@ens-lyon.fr>
 %  - Benjamin Girault <benjamin.girault@usc.edu>
 
-% Copyright Benjamin Girault, École Normale Supérieure de Lyon, FRANCE /
-% Inria, FRANCE (2015-11-01)
 % Copyright Benjamin Girault, University of Sourthern California, Los
 % Angeles, California, USA (2017-2018)
 % 
-% benjamin.girault@ens-lyon.fr
 % benjamin.girault@usc.edu
 % 
 % This software is a computer program whose purpose is to provide a Matlab
@@ -48,12 +46,32 @@
 % The fact that you are presently reading this means that you have had
 % knowledge of the CeCILL license and that you accept its terms.
 
-function [graph, nodes] = grasp_biggest_connected_component(graph)
-    % Find the biggest connected component
-    [conn_comps, sizes] = components(sparse(graph.A));
-    [~, biggest_cc] = max(sizes);
-    nodes = find(conn_comps == biggest_cc);
+function [g, mapping] = grasp_subgraph(g, vertex_set)
+    Nold = grasp_nb_nodes(g);
+    Nnew = numel(vertex_set);
     
-    % Select only the nodes of that component
-    graph = grasp_subgraph(graph, nodes);
+    mapping = zeros(Nold, 1);
+    mapping(vertex_set) = (1:Nnew)';
+    
+    if numel(g.A) > 1
+        g.A = g.A(vertex_set, vertex_set);
+    end
+    if numel(g.A_layout) > 1
+        g.A_layout = g.A_layout(vertex_set, vertex_set);
+    end
+    if numel(g.layout) > 1
+        g.layout = g.layout(vertex_set, :);
+    end
+    if numel(g.distances) > 1
+        g.distances = g.distances(vertex_set, vertex_set);
+    end
+    if numel(g.node_names) > 0
+        g.node_names = g.node_names(vertex_set);
+    end
+    g.L = 0;
+    g.fourier_version = 'n.a.';   % Matrix used for the Fourier transform
+    g.eigvals = 0;                % Eigenvalues of the graph
+    g.F = 0;                      % Fourier matrix
+    g.Finv = 0;                   % Invert Fourier matrix
+    g.T = 0;                      % Translation operator
 end
--- RELEASENOTES.md
+++ RELEASENOTES.md
@@ -26,6 +26,7 @@
  * `grasp_plane_rnd`: options to control the threshold on weights of displayed edges.
  * `grasp_show_fouriermodes`: pass along options to `grasp_show_graph`.
  * `grasp_adjacency_gaussian`: degree-normalized weights.
+ * `grasp_subgraph`: perform a subgraph computation from a subset of vertices.
 
 ### Bugfixes
 
